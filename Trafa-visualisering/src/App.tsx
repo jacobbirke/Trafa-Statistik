2d5086eeae20de1d45bcb28e52aa4131f088b2d1
jacobbirke <birkejacob@gmail.com>
2024-11-19 16:26:20 +0100

    jsfiddle

--- Trafa-visualisering/src/App.tsx
+++ Trafa-visualisering/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DataVisualization from './components/DataVisualization';
+import InputDiagram from './components/InputDiagram';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/statistik" element={<DataVisualization />} />
+        <Route path="/diagram" element={<InputDiagram />} />
       </Routes>
     </Router>
   );
