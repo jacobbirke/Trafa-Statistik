984316ec5792c88bef39affbf35b95817d169e13
Jacob Birke <birkejacob@gmail.com>
2025-01-08 09:14:51 +0100

    undo

--- Trafa-visualisering/src/main.tsx
+++ Trafa-visualisering/src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.tsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 // import PunktlighetTagtyper from './components/PunktlighetTagtyper.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <SpeedInsights/>
     {/* <PunktlighetTagtyper/> */}
   </StrictMode>,
 )
